bdb3b252c61c8bf12cfa368fe4782796ae77e88e
TK <peh3@users.noreply.github.com>
2026-06-20 11:53:35 +0800

    add home

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, TK!</p>"
 
+@app.route("/home")
+def home():
+    return "<p>Welcome Home!</p>"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
