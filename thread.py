e0035fe352966e02c9f7ad3e08e0d086057b0f9e
TK <peh3@users.noreply.github.com>
2026-06-20 12:08:58 +0800

    add thread

---
+++ thread.py
@@ -0,0 +1,22 @@
+import threading
+# Simple web application using Flask framework
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, TK!</p>"
+
+@app.route("/home")
+def home():
+    return "<p>Welcome Home!</p>"
+
+@app.route("/thread-check")
+def thread_check():
+    # Returns the name of the thread handling this specific request
+    current_thread = threading.current_thread().name
+    return f"<p>Active Request Thread Name: {current_thread}</p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
